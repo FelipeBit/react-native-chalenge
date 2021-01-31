b4c05727a9beb48b73625d818f650a8f885a0d04
felipebit <felipebit>
2021-01-31 00:09:14 -0300

    finished

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const ip = '192.168.0.103'
+
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: `http://${ip}:3333`,
 });
 
 export default api;
